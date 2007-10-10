56d32a0adee2c9cfe547d451360e949e096c6ac3
josch <josch@7eef14d0-6ed0-489d-bf55-20463b2d70db>
2007-10-10 13:25:41 +0000

    extended login with database
    git-svn-id: http://yolanda.mister-muffin.de/svn@12 7eef14d0-6ed0-489d-bf55-20463b2d70db

--- trunk/login.pl
+++ trunk/login.pl
@@ -1,8 +1,5 @@
 require "/var/www/perl/include.pl";
 
-CGI::Session->name($session_name);
-my $session = new CGI::Session;
-
 #fill %querystring with everything that was passed via GET
 @parts = split( /\&/, $ENV{ "QUERY_STRING" } );
 foreach $part (@parts) {
@@ -10,17 +7,53 @@ foreach $part (@parts) {
 	$queryString{ $name } = $value;
 }
 
+#fill %querystring with everything that was passed via POST
+read( STDIN, $tmpStr, $ENV{ "CONTENT_LENGTH" } );
+@parts = split( /\&/, $tmpStr );
+foreach $part (@parts) {
+	( $name, $value ) = split( /\=/, $part );
+	$queryString{ $name } = $value;
+}
+
+CGI::Session->name($session_name);
+my $session = new CGI::Session;
+
 if($queryString{ "action" }) {
 	if($queryString{ "action" } eq "login") {
-		$session->param('auth', 'true');
-		print $session->header();
-		print "logged in";
+		$dbh = DBI->connect("DBI:mysql:$database:$host", $user, $pass);
+		my $sth = $dbh->prepare(qq{select username from users
+				where password = password('$queryString{ "pass" }')
+				and username = '$queryString{ "user" }'
+				limit 1 });
+		$sth->execute();
+		
+		if($sth->fetchrow_array()) {
+			$session->param('auth', 'true');
+			print $session->header();
+			print "logged in";
+		} else {
+			print $session->header();
+			print $queryString{ "action" };
+		}
+		
+		$sth->finish();
+		$dbh->disconnect();
+		
 	} elsif($queryString{ "action" } eq "logout") {
 		$session->param('auth', 'false');
 		print $session->header();
 		print "logged out";
+	} else {
+		print $session->header();
+		print "wtf?";
 	}
 } else {
 	print $session->header();
-	print "incorrect query string";
+	print '<form action="" method="POST"><p>
+<input name="action" type="hidden" value="login">
+<input name="user" type="text" size="30" maxlength="30">
+<input name="pass" type="password" size="30" maxlength="30">
+<input type="submit" name="login" value=" login ">
+</p></form>';
+	print STDIN;
 }
